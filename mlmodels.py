97e4a7199b9d7ffbc72d64e08f496366cab14584
migueliglalc <100429148@alumnos.uc3m.es>
2024-02-22 17:32:53 +0100

    pytorch

--- mlmodels.py
+++ mlmodels.py
@@ -1,17 +1,26 @@
 from sklearn.linear_model import LogisticRegression as LRBase
 from sklearn.ensemble import RandomForestClassifier as RFBase
 from sklearn.metrics import accuracy_score, precision_score, roc_auc_score, recall_score, f1_score, average_precision_score
-from tensorflow.keras import models
-from tensorflow.keras.layers import Conv1D, Flatten, Dense
-from sklearn.utils.class_weight import compute_class_weight
+#from tensorflow.keras import models
+#from tensorflow.keras.layers import Conv1D, Flatten, Dense
+#from sklearn.utils.class_weight import compute_class_weight
 import numpy as np
-import tensorflow as tf
+#import tensorflow as tf
+#from tensorflow.keras.callbacks import EarlyStopping
+import torch.nn as nn
+import numpy as np
+import torch
+import torch.nn.functional as F
+from torch.utils.data import DataLoader, TensorDataset
+import torch.optim as optim
+
+device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
 
 class MLModel:
     def __init__(self, **config):
         pass
     
-    def fit(self, X_train, y_train):
+    def fit(self, X_train, y_train, X_val, y_val):
         pass
 
     def predict(self, X_val):
@@ -36,7 +45,7 @@ class LogisticRegression(MLModel):
         super().__init__()
         self.model = LRBase(random_state=42, penalty=config['penalty'], class_weight=config['class_weight'])
 
-    def fit(self, X_train, y_train):
+    def fit(self, X_train, y_train, X_val, y_val):
         self.model.fit(X_train, y_train)
     
     def predict(self, X_val):
@@ -50,7 +59,7 @@ class RandomForest(MLModel):
         super().__init__()
         self.model = RFBase(random_state=42, n_estimators=config["n_estimators"], class_weight=config["class_weight"])
 
-    def fit(self, X_train, y_train):
+    def fit(self, X_train, y_train, X_val, y_val):
         self.model.fit(X_train, y_train)
     
     def predict(self, X_val):
@@ -58,13 +67,14 @@ class RandomForest(MLModel):
     
     def predict_proba(self, X_val):
         return self.model.predict_proba(X_val)[:, 1]
-    
+
+"""
 class MLP_TF(MLModel):
     def __init__(self, config):
         super().__init__()
         self.config = config
 
-    def fit(self, X_train, y_train):
+    def fit(self, X_train, y_train, X_val, y_val):
         model = models.Sequential()
         model.add(Dense(self.config["hidden_dim"], activation=self.config["hidden_act"], input_dim=X_train.shape[1]))
         for _ in range(self.config["n_layers"]):
@@ -76,18 +86,21 @@ class MLP_TF(MLModel):
         class_weight[0], class_weight[1]  = compute_class_weight(class_weight=self.config["class_weight"], classes=np.unique(y_train), y=y_train)
         self.model.fit(X_train, y_train, epochs=100, class_weight=class_weight, verbose=0)
 
+        early_stopping = EarlyStopping(monitor='val_loss', patience=5, restore_best_weights=True)
+        self.model.fit(X_train, y_train, epochs=200, validation_data=(X_val, y_val), class_weight=class_weight, verbose=0, callbacks=[early_stopping])
+
     def predict(self, X_val):
         return tf.round(self.model.predict(X_val))
     
     def predict_proba(self, X_val):
         return self.model.predict(X_val)
-    
+
 class CNN_TF(MLModel):
     def __init__(self, config):
         super().__init__()
         self.config = config
 
-    def fit(self, X_train, y_train):
+    def fit(self, X_train, y_train, X_val, y_val):
         model = models.Sequential()
         model.add(Conv1D(self.config["hidden_dim"], kernel_size=self.config["kernel_size"], activation=self.config["hidden_act"], input_shape=X_train.shape))
         model.add(Flatten())
@@ -96,10 +109,142 @@ class CNN_TF(MLModel):
         self.model = model
         class_weight = {}
         class_weight[0], class_weight[1]  = compute_class_weight(class_weight=self.config["class_weight"], classes=np.unique(y_train), y=y_train)
-        self.model.fit(X_train, y_train, epochs=100, class_weight=class_weight, verbose=0)
+
+        early_stopping = EarlyStopping(monitor='val_loss', patience=5, restore_best_weights=True)
+        self.model.fit(X_train, y_train, epochs=200, validation_data=(X_val, y_val), class_weight=class_weight, verbose=0, callbacks=[early_stopping])
     
     def predict(self, X_val):
         return tf.round(self.model.predict(X_val))
     
     def predict_proba(self, X_val):
         return self.model.predict(X_val)
+"""
+        
+        
+class MLP(nn.Module):
+    def __init__(self, input_size, hidden_size, num_layers, output_size, activation):
+        super(MLP, self).__init__()
+
+        if num_layers == 0:
+            self.fc_input = nn.Linear(input_size, output_size)
+        else:
+            self.fc_input = nn.Linear(input_size, hidden_size)
+            self.hidden_layers = nn.ModuleList([
+                nn.Linear(hidden_size, hidden_size) for _ in range(num_layers - 1)
+            ])
+            self.fc_output = nn.Linear(hidden_size, output_size)
+            if activation == "leakyrelu":
+                self.activation = nn.LeakyReLU()
+            elif activation == "sigmoid":
+                self.activation = nn.Sigmoid()
+            elif activation == "tanh":
+                self.activation = nn.Tanh()
+            elif activation == "relu":
+                self.activation = nn.ReLU()
+            elif activation == "linear":
+                self.activation = lambda x: x
+            
+    def forward(self, x):
+        if hasattr(self, 'fc_output'):  # Si hay capas ocultas
+            x = self.activation(self.fc_input(x))
+            for layer in self.hidden_layers:
+                x = self.activation(layer(x))
+            x = self.fc_output(x)
+        else:  # Si no hay capas ocultas
+            x = self.fc_input(x)
+        return x
+
+class Pytorch_Model(MLModel):
+    def __init__(self, config, model):
+        super().__init__()
+        self.config = config
+        self.model = model
+
+    def fit(self, X_train, y_train, X_val, y_val):
+        model = self.model(6000, self.config.hidden_dim, self.config.n_layers, 1, self.config.hidden_act)
+        
+        y_val = torch.from_numpy(y_val).to(device, dtype = torch.float32)
+        X_val = torch.from_numpy(X_val).to(device, dtype = torch.float32)
+        y_train = torch.from_numpy(y_train).to(device, dtype = torch.float32)
+        X_train = torch.from_numpy(X_train).to(device, dtype = torch.float32)
+
+        num_positives = torch.sum(y_train, dim=0)
+        num_negatives = len(y_train) - num_positives
+        pos_weight  = num_negatives / num_positives
+
+        epochs = 200
+        patience = 5
+        batch_size = 128
+
+        optimizer = optim.Adam(model.parameters(), lr=0.0005)
+        criterion = nn.BCEWithLogitsLoss(pos_weight)
+
+        train_set_fold = TensorDataset(X_train, y_train)
+        val_set_fold = TensorDataset(X_val, y_val)
+
+        train_loader = DataLoader(train_set_fold, batch_size=batch_size, shuffle=True, num_workers=16)
+        val_loader = DataLoader(val_set_fold, batch_size=batch_size, num_workers=16)
+
+        train_losses = []
+        val_losses = []
+        best_val_loss = np.inf
+
+        counter = 0
+
+        for epoch in range(epochs):
+            model.train()
+            train_loss = 0.0
+            for batch_idx, instances in enumerate(train_loader):
+                input = instances[0].to(device, dtype = torch.float32)
+                labels = instances[1].to(device, dtype = torch.float32)
+                labels = labels.unsqueeze(1)
+                optimizer.zero_grad(set_to_none = True)
+                predictions = model(input)
+                loss = criterion(predictions, labels)
+
+                loss.backward()
+                optimizer.step()
+
+                train_loss += loss.item()
+                    
+            avg_train_loss = train_loss/len(train_loader)
+            train_losses.append(avg_train_loss)
+
+            avg_val_loss,_ = self.validate(model, val_loader, criterion, device)
+            if avg_val_loss<=best_val_loss:
+                counter = 0
+                #torch.save(model.state_dict(), f"models/{name}.pth")
+                best_val_loss = avg_val_loss
+            else:
+                counter+=1
+                if counter >= patience:
+                    #print("Deteniendo el entrenamiento debido a early stopping.")
+                    break
+
+            #scheduler.step(avg_val_loss)
+
+            val_losses.append(avg_val_loss)
+            #print(f"Epoch [{epoch + 1}/{epochs}] - Train Loss: {avg_train_loss:.4f}, Val Loss: {avg_val_loss:.4f}\n")
+            #wandb.log({"train_loss": avg_train_loss, "val_loss": avg_val_loss, "epoch": epoch+1})
+
+        return train_losses, val_losses
+
+    def validate(model, val_loader, criterion, device, name):
+        predictions = []
+        with torch.no_grad():
+            model.eval()
+            val_loss = 0.0
+            for instances in val_loader:
+                input = instances[0].to(device, dtype = torch.float32)
+                labels = instances[1].to(device, dtype = torch.float32)
+                labels = labels.unsqueeze(1)
+                pred = model(input)
+                loss = criterion(pred, labels)
+                loss = criterion(pred, input)
+                predictions.append(pred)
+                val_loss += loss.item()
+            
+        avg_val_loss = val_loss / len(val_loader)
+        return avg_val_loss, predictions
+
+
--- sweep_clean.py
+++ sweep_clean.py
@@ -1,17 +1,22 @@
 from sklearn.model_selection import StratifiedKFold
+from sklearn.preprocessing import Normalizer
 from mlmodels import LogisticRegression, RandomForest, MLP_TF, CNN_TF
+from imblearn.over_sampling import SMOTE, RandomOverSampler
 
 import numpy as np
 import tensorflow as tf
 import pandas as pd
 import wandb
 import yaml
+import joblib
 
-CONFIG_FILE = "./sweep_configs/MLP_Erythromycin.yaml"
 MODEL = MLP_TF
 ANTIBIOTIC = "Erythromycin"
+ALGORITHM = 'MLP'
+CONFIG_FILE = f"./sweep_configs/{ALGORITHM}_{ANTIBIOTIC}.yaml"
 N_SPLITS = 5
-
+RESAMPLER = 'GAN'
+SCALING = Normalizer(norm='l2')
 
 def setup_dataset():
     df = pd.read_csv(f"./data/train_{ANTIBIOTIC[0]}.csv")
@@ -20,6 +25,8 @@ def setup_dataset():
     return X, y
 
 X, y = setup_dataset()
+if SCALING != 'None':
+    X = SCALING.fit_transform(X)
 
 def train():
     run = wandb.init()
@@ -29,11 +36,31 @@ def train():
 
     for train_index, val_index in skf.split(X, y):
         X_train, X_val, y_train, y_val = X[train_index], X[val_index], y[train_index], y[val_index]
+        X_train, y_train = resample(X_train, y_train)
         model = MODEL(config)
-        model.fit(X_train, y_train)
+        model.fit(X_train, y_train, X_val, y_val)
         metrics = model.evaluate(X_val, y_val, metrics)
     for metric, metric_list in metrics.items():
         wandb.log({metric: np.mean(metric_list)})
+
+def resample(x, y):
+    num_1 = np.sum(x == 1)
+    num_0 = np.sum(y == 0)
+    missing_samples = num_0 - num_1
+    if RESAMPLER == 'GAN':
+        fake = joblib.load(f"fake_{ANTIBIOTIC[0]}.pkl")
+        indices = np.random.permutation(fake.shape[0])[:missing_samples]
+        selected_fake = fake[indices]
+        labels = np.ones(selected_fake.shape[0])
+        x = np.concatenate([x, selected_fake], axis=0)
+        y = np.concatenate([y, labels], axis=0)
+    elif RESAMPLER == 'SMOTE':
+        smote = SMOTE(random_state = 42)
+        x, y = smote.fit_resample(x, y)
+    elif RESAMPLER == 'RANDOM':
+        ros = RandomOverSampler(random_state=42)
+        x, y = ros.fit_resample(x, y)
+    return x, y
 
 
 if __name__ == "__main__":
